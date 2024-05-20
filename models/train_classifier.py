bc0464aa71a86e3ad6a823a372718e0bd18c5774
ginn cheng <ginn.z.cheng@gmail.com>
2024-05-20 18:37:51 +0800

    feat: update ML pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,7 +1,46 @@
+"""
+train_classifiers.py
+
+This script trains a machine learning model to classify disaster messages into multiple categories.
+The model is trained using a pipeline that includes text preprocessing (tokenization, lemmatization),
+feature extraction (TF-IDF), and a multi-output classifier (RandomForestClassifier).
+
+The script performs the following steps:
+1. Load and clean the dataset.
+2. Define a machine learning pipeline.
+3. Train the model using GridSearchCV to find the best parameters.
+4. Evaluate the model on a test set.
+5. Save the trained model to a file for future use.
+
+Functions:
+-----------
+- load_data(database_filepath): Load the dataset from the SQLite database.
+- tokenize(text): Preprocess text by normalizing, tokenizing, and lemmatizing.
+- build_model(): Build a machine learning pipeline and perform a grid search.
+- evaluate_model(model, X_test, Y_test, category_names): Evaluate the model and print the classification report.
+- save_model(model, model_filepath): Save the trained model to a pickle file.
+- main(): Main function to run the script.
+
+Parameters:
+-----------
+- database_filepath (str): Filepath of the SQLite database containing the cleaned dataset.
+- model_filepath (str): Filepath where the trained model will be saved.
+
+Usage:
+------
+To run the script from the command line:
+$ python train_classifiers.py <database_filepath> <model_filepath>
+
+Example:
+--------
+$ python models/train_classifier.py data/sql_database.db models/disaster_response_model.pkl
+"""
+
+
 import sys
 from sqlalchemy import create_engine
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from nltk.stem import WordNetLemmatizer
 import re
 from nltk.tokenize import word_tokenize
@@ -32,39 +71,40 @@ def tokenize(text):
     tokenized_text = [lemmatizer.lemmatize(token) for token in tokenized_text]
     return tokenized_text
 
-def build_model():
-    from sklearn.feature_extraction.text import HashingVectorizer
-    # set the pipeline using HashingVectorizer
-    model = Pipeline([
-        ('vect', TfidfVectorizer(tokenizer=tokenize, max_df=0.75)),
+def build_model(X_train, Y_train):
+    # setup the pipeline
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=30, min_samples_split=2)))
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
+    # setup the grid search
+    parameters = {
+        'clf__estimator__n_estimators': [5, 10],
+        'clf__estimator__min_samples_split': [2, 4]
+    }
+    cv = 3
+    grid_search = GridSearchCV(pipeline, param_grid=parameters, cv=cv)
+    print('Training model...')
+    grid_search.fit(X_train, Y_train)
+    # Print best parameters
+    print("Best parameters found:")
+    print(grid_search.best_params_)
+    # Use the best estimator to make predictions
+    best_pipeline = grid_search.best_estimator_
 
-    return model
+    return best_pipeline
 
 # setup dataframes for predicting 0 and 1 of the message
-def f1_report_frame(category_names, y_test, y_pred, score_name='f1-score'):
-    if score_name in ['f1-score', 'recall', 'precision']:
-        f1_report = {}
-        for i, col in enumerate(category_names):
-            # Generate classification report as dictionary
-            tmp_rpt = classification_report(y_test.iloc[:,i], y_pred[:,i], output_dict=True)
-            # Extract F1-scores for each label
-            f1_scores = {label: metrics[score_name] for label, metrics in tmp_rpt.items() if label not in ['accuracy', 'macro avg', 'weighted avg']}
-            f1_report[col] = f1_scores
-        print('The ' + score_name + ' is:')
-        print(pd.DataFrame(f1_report))
-    else:
-        print("The score name need to be one of the three - 'f1-score','recall', or 'precision'")
+def report_frame(category_names, y_test, y_pred):
+    for i, col in enumerate(category_names):
+        print(f"Category: {col}\n", classification_report(y_test[col], y_pred[:, i]))
 
 def evaluate_model(model, X_test, y_test, category_names):
     # Evaluate the model
     y_pred = model.predict(X_test)
     # check the f1 score report
-    f1_report_frame(category_names, y_test, y_pred)
-    f1_report_frame(category_names, y_test, y_pred, score_name='recall')
-    f1_report_frame(category_names, y_test, y_pred, score_name='precision')
+    report_frame(category_names, y_test, y_pred)
 
 def save_model(model, model_filepath):
     import joblib
@@ -79,10 +119,10 @@ def main():
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
-        model = build_model()
+        model = build_model(X_train, Y_train)
         
-        print('Training model...')
-        model.fit(X_train, Y_train)
+        # print('Training model...')
+        # model.fit(X_train, Y_train)
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
